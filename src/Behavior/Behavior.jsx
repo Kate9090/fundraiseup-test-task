88e5c6501ee9d9fea292690c744d5497dfd68b84
Ekaterina <vam.pochta@yahoo.com>
2023-03-27 17:37:18 +0400

    edits attributes names

--- src/Behavior/Behavior.jsx
+++ src/Behavior/Behavior.jsx
@@ -6,7 +6,7 @@ import styles from './Behavior.module.scss';
 
 const Arrow = () => (
   <svg width="11" height="6" viewBox="0 0 11 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M1 0.75L5.5 5.25L10 0.75" stroke="#6C6D6F" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M1 0.75L5.5 5.25L10 0.75" stroke="#6C6D6F" strokeLinecap="round" strokeLinejoin="round"/>
   </svg>
 )
 
@@ -30,7 +30,7 @@ const Behavior = () => {
   return (
     <form className={styles.Behavior__Form}>
       <fieldset className={styles.Behavior__InputGroup}>
-        <label for="setting" className={styles.Behavior__Label}>Designation</label>
+        <label htmlFor="setting" className={styles.Behavior__Label}>Designation</label>
         <div className={styles.Behavior__SelectWrapper}>
           <select id="setting" className={cx(styles.Behavior__Select, styles.Behavior__Wide)}>
             <option>Match Checkout Setting</option>
@@ -41,13 +41,15 @@ const Behavior = () => {
         </div>
       </fieldset>
       <fieldset className={styles.Behavior__InputGroup}>
-        <label for="currency" className={styles.Behavior__Label}>Goal</label>
+        <label htmlFor="currency" className={styles.Behavior__Label}>Goal</label>
         <div className={cx(styles.Behavior__Row, styles.Behavior__Goal)}>
           <div className={styles.Behavior__InputWrapper}>
             <span  className={styles.Behavior__Icon}>
               $
             </span>
-            <input type="text" id="currency" defaultValue={'10.00'} className={styles.Behavior__Amount}/>
+            <input type="number" step="0.01" min="0" pattern="[0-9]+([\.,][0-9]+)?" id="currency" defaultValue={'10.00'}
+              className={styles.Behavior__Amount}
+            />
           </div>
           
           <div className={cx(styles.Behavior__SelectWrapper, styles['Behavior__SelectWrapper--currency'])}>
@@ -67,7 +69,7 @@ const Behavior = () => {
           <div className={cx(styles.Behavior__Row, styles.Behavior__WithBottom)}>
             <input type="radio" id="choice1"
               name="choice" value="Checkout" />
-            <label for="choice1" className={styles.Behavior__RadioLabel}>
+            <label htmlFor="choice1" className={styles.Behavior__RadioLabel}>
               Match Checkout Setting
             </label>
           </div>
@@ -75,18 +77,18 @@ const Behavior = () => {
           <div className={cx(styles.Behavior__Row, styles.Behavior__WithBottom)}>
             <input type="radio" id="choice2" defaultChecked={true}
               name="choice" value="Customize" />
-            <label for="choice2" className={styles.Behavior__RadioLabel}>Customize</label>
+            <label htmlFor="choice2" className={styles.Behavior__RadioLabel}>Customize</label>
           </div>
 
           <div className={styles.Behavior__Row}>
             <input type="checkbox" checked={checked} onChange={handleChangeCheck}
               name="Allow" value="Allow" id="allow" />
-            <label className={styles.Behavior__CheckboxLabel} for="allow">Allow donor to change default currency</label>
+            <label className={styles.Behavior__CheckboxLabel} htmlFor="allow">Allow donor to change default currency</label>
           </div>
         </div>
       </fieldset>
       <fieldset className={styles.Behavior__InputGroup}>
-        <label for="border" className={styles.Behavior__Label}>Border size</label>
+        <label htmlFor="border" className={styles.Behavior__Label}>Border size</label>
         <div className={styles.Behavior__SliderWrapper}>
           <input type="range" id="border" name="volume"
                 min="-1" max="3.6" value={border} onChange={e => handleChangeBorder(e)} />
@@ -94,7 +96,7 @@ const Behavior = () => {
         </div>
       </fieldset>
       <fieldset className={styles.Behavior__InputGroup}>
-        <label for="radius" className={styles.Behavior__Label}>Border radius</label>
+        <label htmlFor="radius" className={styles.Behavior__Label}>Border radius</label>
         <div className={styles.Behavior__SliderWrapper}>
           <input type="range" id="radius" name="volume2"
                 min="0" max="23" value={radius} onChange={e => handleChangeRadius(e)} 
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -4,7 +4,7 @@ import styles from './Header.module.scss';
 
 const Arrow = () => (
   <svg width="11" height="6" viewBox="0 0 11 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M1 0.75L5.5 5.25L10 0.75" stroke="#6C6D6F" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M1 0.75L5.5 5.25L10 0.75" stroke="#6C6D6F" strokeLinecap="round" strokeLinejoin="round"/>
   </svg>
 )
 
@@ -16,11 +16,11 @@ const Header = () => {
         <div className={styles.Header__InputWrapper}>
           <span  className={styles.Header__Icon}>
             <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M8.25 14.25C11.5637 14.25 14.25 11.5637 14.25 8.25C14.25 4.93629 11.5637 2.25 8.25 2.25C4.93629 2.25 2.25 4.93629 2.25 8.25C2.25 11.5637 4.93629 14.25 8.25 14.25Z" stroke="#6C6D6F" stroke-linecap="round" stroke-linejoin="round"/>
-              <path d="M15.7499 15.7499L12.4874 12.4874" stroke="#6C6D6F" stroke-linecap="round" stroke-linejoin="round"/>
+              <path d="M8.25 14.25C11.5637 14.25 14.25 11.5637 14.25 8.25C14.25 4.93629 11.5637 2.25 8.25 2.25C4.93629 2.25 2.25 4.93629 2.25 8.25C2.25 11.5637 4.93629 14.25 8.25 14.25Z" stroke="#6C6D6F" strokeLinecap="round" strokeLinejoin="round"/>
+              <path d="M15.7499 15.7499L12.4874 12.4874" stroke="#6C6D6F" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
           </span>
-          <input placeholder={'Search'} type="search" autoFocus/>
+          <input placeholder={'Search'} type="search" autoFocus />
         </div>
         <div className={styles.Header__SelectWrapper}>
           <select placeholder={'All statuses'}>
